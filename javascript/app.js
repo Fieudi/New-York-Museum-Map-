54d0b67b67d096121632810a5c384e0aff4cab1d
fieudi <fieudiwen@gmail.com>
2016-12-14 16:02:01 -0500

    fix bug

--- javascript/app.js
+++ javascript/app.js
@@ -321,9 +321,9 @@ var weatherUrl = "http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/
 $.getJSON(weatherUrl, function(data){
 	var list = $('#weatherInfor');
 	var weatherInfor = data.current_observation;
-	list.appendChild('<div>' + weatherInfor.observation_time + '</div>'); 
-	list.appendChild('<div>Weahter : ' + weatherInfor.temperature_string + ' ° F</div>');
-	list.appendChild('<div><img style="width: 24px" src="' + weatherInfor.icon_url + '">  ' + weatherInfor.icon + '</div>');
+	list.append('<div>' + weatherInfor.observation_time + '</div>'); 
+	list.append('<div>Weahter : ' + weatherInfor.temperature_string + ' ° F</div>');
+	list.append('<div><img style="width: 24px" src="' + weatherInfor.icon_url + '">  ' + weatherInfor.icon + '</div>');
 }).fail(function(e){
 	$('#weather').append('<p style="text-align: center;">Sorry! Weather Underground</p><p style="text-align: center;">Could Not Be Loaded</p>');
 })
